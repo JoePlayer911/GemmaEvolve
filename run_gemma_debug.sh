ee8649cd2f34a00aa8b911ef765909d8bb0f3d59
JoePlayer911 <jnthnie@gmail.com>
2026-01-11 15:18:43 +0800

    Updated Island Database setup

--- run_gemma_debug.sh
+++ run_gemma_debug.sh
@@ -14,10 +14,9 @@ echo
   examples/circle_packing/initial_program.py \
   examples/circle_packing/evaluator.py \
   --config examples/circle_packing/gemma_config.yaml \
-  --iterations 5 \
+  --iterations 50 \
   --log-level DEBUG \
-  
-# --checkpoint examples/circle_packing/openevolve_output/checkpoints/checkpoint_5
+  --checkpoint examples/circle_packing/openevolve_output/checkpoints/checkpoint_50
 
 echo
 echo "========================================"
